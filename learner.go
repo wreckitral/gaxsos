fb6906ee9eb44164c6230a3f8f1791dae6eaa5dc
wreckitral <defhanayasofhiea@gmail.com>
2024-12-04 01:05:06 +0700

    feat: create learner

---
+++ learner.go
@@ -0,0 +1,37 @@
+package gaxsos
+
+import "fmt"
+
+type learner struct {
+    id int // identifier for learner
+    receives chan message // channel to listen to chosen value
+}
+
+func NewLearner(id int, receives chan message) *learner {
+    l := learner{}
+
+    l.id = id
+    l.receives = receives
+
+    return &l
+}
+
+func (l *learner) Run() string {
+    val := "zero" // default value
+
+    for val == "zero" {
+        msg := <- l.receives
+
+        switch msg.status {
+        case Chosen:
+            val = msg.proposalVal
+
+        default:
+        }
+    }
+
+    fmt.Printf("Learner %v: Chosen %v", l.id, val)
+
+    return val
+}
+
