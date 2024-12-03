67cbb79f9c24dfe2224f330027c74f3d7f5eb90f
wreckitral <defhanayasofhiea@gmail.com>
2024-12-04 00:15:33 +0700

    fix(proposer): fix struct naming

--- proposer.go
+++ proposer.go
@@ -3,8 +3,8 @@ package gaxsos
 type proposer struct {
     id          int // the identifier for each proposer
 
-    proposeOrd  int // ordering number for the proposal
-    proposeVal  string // value that intended to be proposed
+    proposalOrd  int // ordering number for the proposal
+    proposalVal  string // value that intended to be proposed
 
     acceptors   []chan message // channels for sending message to acceptors
     receives    chan message // channel for receiving messages from acceptors
